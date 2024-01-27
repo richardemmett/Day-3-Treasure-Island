a915413b0c43ec67d4e78b4d42c60a03f8a56ee8
richardemmett <157606947+richardemmett@users.noreply.github.com>
2024-01-27 14:21:23 +0000

    First commit

--- main.py
+++ main.py
@@ -21,30 +21,35 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
+print("Your mission is to find the treasure.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-left_right = input("You are at a crossroad. Where do you want to go? Type 'left' or 'right'")
+left_right = input(
+    "You are at a crossroad. Where do you want to go? Type 'left' or 'right'")
 left_right = left_right.lower()
 if left_right == "left":
-  #Continue
-  swim_wait = input("You come to a lake. There is an island in the middle of the lake. Type 'swim' or 'wait'")
-  swim_wait = swim_wait.lower()
-  if swim_wait == "wait":
     #Continue
-    door = input("You arrive at the island unharmed. There is a house with 3 doors.Choose 'red', 'yellow' or 'blue'")
-    door = door.lower()
-    if door == "red":
-      print ("Burned by fire. Game over.")
-    elif door == "yellow":
-      print ("You win!")
-    elif door == "blue":
-      print ("Eaten by beasts. Game over.")
+    swim_wait = input(
+        "You come to a lake. There is an island in the middle of the lake. Type 'swim' or 'wait'"
+    )
+    swim_wait = swim_wait.lower()
+    if swim_wait == "wait":
+        #Continue
+        door = input(
+            "You arrive at the island unharmed. There is a house with 3 doors.Choose 'red', 'yellow' or 'blue'"
+        )
+        door = door.lower()
+        if door == "red":
+            print("Burned by fire. Game over.")
+        elif door == "yellow":
+            print("You win!")
+        elif door == "blue":
+            print("Eaten by beasts. Game over.")
+        else:
+            print("Game Over.")
     else:
-      print ("Game Over.")
-  else:
-    print("You were attacked by a trout. Game over.")
+        print("You were attacked by a trout. Game over.")
 else:
-  print("You fell into a hole. Game over.")
+    print("You fell into a hole. Game over.")
